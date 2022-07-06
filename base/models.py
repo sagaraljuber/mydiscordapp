e00a6f0fee3a5a7fa6245416486ef616e601fd7d
Juber Sagaral <sagaraljuber7@gmail.com>
2022-07-06 15:47:48 +0800

    edit room in views

--- base/models.py
+++ base/models.py
@@ -10,6 +10,6 @@ class Room(models.Model):
     #participants = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.name

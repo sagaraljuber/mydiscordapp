03f3fe7cf213d98607f048bf9c56b62baf67ff05
Juber Sagaral <sagaraljuber7@gmail.com>
2022-07-09 23:08:17 +0800

    add topics,messages and models

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room
+from .models import Room, Topic, Message
 
 #Create authentication and authorization
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)

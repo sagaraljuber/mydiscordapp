163544dde317f00ba96341d9c15cb049e0d8ea78
Juber Sagaral <sagaraljuber7@gmail.com>
2022-07-22 01:07:37 +0800

    debugging Message and Adding updateRoom

--- base/models.py
+++ base/models.py
@@ -20,11 +20,14 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-updated', '-created']
+    
     def __str__(self):
         return self.name
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
@@ -32,3 +35,5 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+
+
--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -23,5 +24,26 @@ def room(request, pk):
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()  
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
 
     
